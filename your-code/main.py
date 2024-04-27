ba7b9244567d3c317c06842627020b609d58557d
ppalomoag97 <patricia.palomo.aguilar@gmail.com>
2024-04-27 11:04:19 +0200

    lab6

--- your-code/main.py
+++ your-code/main.py
@@ -1,71 +1,81 @@
 #1. Importa el paquete NUMPY bajo el nombre np.
-
+import os
 import numpy as np
-
-
 #2. Imprime la versión de NUMPY y la configuración.
-
-#[tu código aquí]
-
+print(np.version.version)
 
 #3. Genera un array tridimensional de 2x3x5 con valores aleatorios. Asigna el array a la variable "a"
 # Desafío: hay al menos tres maneras fáciles que usan numpy para generar arrays aleatorios. ¿Cuántas formas puedes encontrar?
 
-#[tu código aquí]
+a1 = np.random.randint(10,size=(2, 3, 5))
+a = np.random.random((2, 3, 5))
 
 #4. Imprime a.
 
-#[tu código aquí]
+print(f"a:{a}")
+
 #5. Crea un array tridimensional de 5x2x3 con todos los valores igual a 1.
 #Asigna el array a la variable "b"
-
-#[tu código aquí]
+b=np.ones((5, 2,3))
 
 #6. Imprime b.
-
-#[tu código aquí]
+print(f"b:{b}")
 
 #7. ¿Tienen a y b el mismo tamaño? ¿Cómo lo demuestras en código Python?
 
-#[tu código aquí]
+tamaño=(lambda a,b: a.size == b.size)(a,b)
+#Tienen el mismo tamaño
 
 #8. ¿Es posible sumar a y b? ¿Por qué sí o por qué no?
-
-#[tu código aquí]
+#a+b
+#No se puede porque no tienen la misma estructura
 
 
 #9. Transpone b para que tenga la misma estructura que a (es decir, se convierta en un array de 2x3x5). Asigna el array transpuesto a la variable "c".
 
-#[tu código aquí]
+c=np.transpose(b,(1,2,0))
 
 #10. Intenta sumar a y c. Ahora debería funcionar. Asigna la suma a la variable "d". Pero, ¿por qué funciona ahora?
 
-#[tu código aquí]
+d=a+c
+#Ahora si funciona porque tienen la misma estructura y por lo tanto cada posicion tiene una posición con la que sumar.
 
 #11. Imprime a y d. ¿Notas la diferencia y la relación entre los dos arrays en términos de los valores? Explica.
 
-#[tu código aquí]
+print(f"a:{a}")
+print(f"d:{d}")
+
+#La diferencia en valores es de 1, ya que le hemos sumado la matriz de unos.
 
 
 #12. Multiplica a y c. Asigna el resultado a e.
 
-#[tu código aquí]
+e=a*c
 
 
 #13. ¿Es e igual a a? ¿Por qué sí o por qué no?
 
-#[tu código aquí]
+print(f"e:{e}")
 
+print(e.shape==a.shape)
+print(np.array_equal(a, e))
+
+#Si que son iguales, a está multiplicada por una matriz de unos con la misma estructura(e), por lo tanto no hay cambios.
 
 
 #14. Identifica los valores máximos, mínimos y medios en d. Asigna esos valores a las variables "d_max", "d_min" y "d_mean"
 
-#[tu código aquí]
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
 
 #15. Ahora queremos etiquetar los valores en d. Primero crea un array vacío "f" con la misma forma (es decir, 2x3x5) que d usando `np.empty`.
 
-#[tu código aquí]
+f = np.empty((2,3,5))
+
+print(f"f:{f}")
+
 
 
 """
@@ -78,9 +88,19 @@ Al final, f debería tener solo los siguientes valores: 0, 25, 50, 75 y 100.
 Nota: no necesitas usar Numpy en esta pregunta.
 """
 
-#[tu código aquí]
-
-
+for i in range(len(d)):
+    for j in range(len(d[i])):
+        for k in range(len(d[i][j])):
+                if d[i][j][k] > d_min and d[i][j][k]  < d_mean: 
+                        f[i][j][k] = 25
+                elif d[i][j][k] > d_mean and d[i][j][k] < d_max: 
+                        f[i][j][k] = 75
+                elif d[i][j][k] == d_mean: 
+                        f[i][j][k] = 50
+                elif d[i][j][k] == d_min: 
+                        f[i][j][k] = 0
+                elif d[i][j][k] == d_max: 
+                        f[i][j][k] = 100
 
 
 """
@@ -103,8 +123,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
-#[tu código aquí]
+print(f"f:{a}")
+print(f"f:{f}")
 
 
 
@@ -120,5 +140,21 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 De nuevo, no necesitas Numpy en esta pregunta.
 """
+g = np.empty((2,3,5),dtype=str)
 
-#[tu código aquí]
+for i in range(len(d)):
+    for j in range(len(d[i])):
+        for k in range(len(d[i][j])):
+                if d[i][j][k] > d_min and d[i][j][k]  < d_mean: 
+                        g[i][j][k] = 'B'
+                elif d[i][j][k] > d_mean and d[i][j][k] < d_max: 
+                        g[i][j][k] = 'C'
+                elif d[i][j][k] == d_mean: 
+                        g[i][j][k] = 'C'
+                elif d[i][j][k] == d_min: 
+                        g[i][j][k] = 'A'
+                elif d[i][j][k] == d_max: 
+                        g[i][j][k] = 'D'
+
+
+print(f"g:{g}")
